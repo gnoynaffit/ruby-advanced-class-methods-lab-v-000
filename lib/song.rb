e4b96666765c8b66592cbcc89e63077668fc4289
Tiffany <ong.tifffany1503@gmail.com>
2019-10-15 22:11:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,3 +11,47 @@ class Song
   end
 
 end
+
+def save
+  self.creat.all << self
+end
+
+def self.create
+  song = self.now
+  song
+end
+
+def self.new_by_name(name)
+  song = self.new_by_name
+  song.name = name
+  song
+end
+
+def self.create_by_name(name)
+  song = self.new_by_name
+  song.name = name
+  @@all << song
+  song
+end
+
+def self.find_by_name(name)
+  @@all.find{|x| x.name == name}
+end
+
+def self.find_find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+end
+
+def self.alphabetical()
+  @@all.sort_by{|x| x.name}
+end
+
+def self.new_from_filename(name)
+  song = self.mew
+  song.name = (name.split(" - ") [1] .chomp(".mp3")) song.artist_name = (name.split{" - "} [0]) song
+end
+
+def self.create_from_filename(name)
+  song = self.new_by_name
+  song.name   
+# Array#sort_by.
