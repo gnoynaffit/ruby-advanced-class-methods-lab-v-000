de9fe9e7555cece92638c6b96ccb3c9a4cd3b131
Tiffany <ong.tifffany1503@gmail.com>
2019-10-15 22:33:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,7 @@ class Song
 
   def self.create
     song = self.new
+    song.save
     song
   end
 
@@ -18,46 +19,54 @@ class Song
   end
 
   def self.create_by_name(name)
-    song = self.new
+    song = self.create
     song.name = name
-    @@all << song
     song
   end
 
   def self.find_by_name(name)
-    @@all.find{|x| x.name == name}
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end
+    end
   end
 
   def self.find_or_create_by_name(name)
-      self.find_by_name(name) || self.create_by_name(name)
-
-    end
-
-  def self.alphabetical()
-    @@all.sort_by{|x| x.name}
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
 
-  def self.new_from_filename(name)
+  def self.alphabetical
+
+    @@all.sort_by{|song| song.name}
+
+  end
+
+  def self.new_from_filename(filename)
+
+    row = filename
+
+      data = row.split(" - ")
+      artist_name = data[0]
+      song_name = data[1].gsub(".mp3", "")
+
     song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
+    song.name = song_name
+    song.artist_name = artist_name
     song
-  end
-
-  def self.create_from_filename(name)
-    song = self.new
-    song.name = (name.split(" - ")[1].chomp(".mp3"))
-    song.artist_name = (name.split(" - ")[0])
-    @@all << song
-    song
-  end
-
-  def self.destroy_all()
-    @@all.clear
-  end
-
 end
 
 
+
+
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end
+
+end
+ ProTip! Use n 
 # Array#sort_by.
